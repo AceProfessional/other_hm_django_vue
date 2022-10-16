2d73eb96ee125b9eb36cb30eb90414e88ae88d42
AliceEngineer <alice_engineer@yeah.net>
2022-10-16 23:53:51 +0800

    功能迁移完善

--- public/urls.py
+++ public/urls.py
@@ -8,11 +8,9 @@
 
 import os, sys
 
-from django.urls.conf import re_path
-from public import views
+from django.urls import re_path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    re_path(route=r'^users/$', view=csrf_exempt(views.UsersView.as_view()), name='users'),
-    re_path(route=r'^users/(?P<uid>\d+)/$', view=csrf_exempt(views.UserView.as_view()), name='user')
+    
 ]
--- study_hm_django_vue/settings.py
+++ study_hm_django_vue/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     # APP
     'public.apps.PublicConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
